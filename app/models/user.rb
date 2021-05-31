6b5d07f22a6ebaea7af4a8da29a76c8885211030
Tyler Bratt <tyler~bratt@hotmail.com>
2021-05-31 18:08:04 -0400

    add specs for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,20 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, on: :create
+  validates :password, presence: true, length: { minimum: 4 }, confirmation: true
+  validates :password_confirmation, presence: true, length: { minimum: 4 }, on: :create
+
   
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
 end
